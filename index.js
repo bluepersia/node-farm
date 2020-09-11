27120ea08e6ca9a4c2fafa2368c71f0b1e12939e
Marco Zimmermann <fuzzyslash@hotmail.com>
2020-09-11 12:09:23 -0600

    basic server

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ const server = http.createServer();
 
 const port = process.env.PORT;
 server.listen(port, () => console.log(`Server started on port ${port}`));
+
+
+
+server.on('request', (req, res) => {
+
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(`<h1>You requested ${req.url}</h1>`);
+});
