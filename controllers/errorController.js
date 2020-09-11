b880edc759d183c311138b192769b8b1d187cb4a
Marco Zimmermann <fuzzyslash@hotmail.com>
2020-09-11 13:09:03 -0600

    Error handler finished

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -6,11 +6,25 @@ module.exports = function (req, res) {
     const { error, url } = req;
 
     if (error) {
-        const statusCode = error.statusCode || 500;
-        const { message } = error;
+        let statusCode = error.statusCode || 500;
+        let { message } = error;
+
+        let rest = {};
+
+        if (process.env.NODE_ENV == 'production') {
+
+            if (!error.isOperational) {
+                statusCode = 500;
+                message = 'Something went wrong!';
+            }
+        }
+        else {
+            rest.error = error;
+            rest.stack = err.stack;
+        }
 
         if (url.startsWith('/api'))
-            jSend(res, statusCode, message);
+            jSend(res, statusCode, message, rest);
         else
             render(res, statusCode, 'error', { message });
     }
